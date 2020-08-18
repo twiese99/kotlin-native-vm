6b77ba4486affbca07641b7f89a25d08a19a943f
Tobias Wiese <38278057+twhx99@users.noreply.github.com>
2020-08-18 16:14:02 +0200

    Fixed MPP build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,21 @@ repositories {
     mavenCentral()
 }
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("macosX64")
-        hostOs == "Linux" -> linuxX64("linuxX64")
-        isMingwX64 -> mingwX64("mingwX64")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    linuxX64 {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
     }
-
-    nativeTarget.apply {
+    mingwX64 {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+    macosX64 {
         binaries {
             executable {
                 entryPoint = "main"
@@ -25,13 +30,18 @@ kotlin {
         }
     }
     sourceSets {
-        val commonMain by getting
-        val commonTest by getting
-        val mingwX64Main by getting
-        val mingwX64Test by getting
         val linuxX64Main by getting
         val linuxX64Test by getting
+        val mingwX64Main by getting
+        val mingwX64Test by getting
         val macosX64Main by getting
         val macosX64Test by getting
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
     }
 }
--- src/macosX64Main/kotlin/main.kt
+++ src/macosX64Main/kotlin/main.kt
@@ -1,3 +1,3 @@
-fun main(args: Array<String>) {
+actual fun main(args: Array<String>) {
     println("Your Operating System is currently not supported")
 }
